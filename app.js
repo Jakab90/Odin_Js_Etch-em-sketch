c86738f4617228a661aa652f38c90c3b7bf959bc
jake <beej8046@gmail.com>
2021-09-20 17:10:53 -0800

    sketchem

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
-let gridNumber = 4;
+let gridNumber = 16;
  
 let gridArea = gridNumber * gridNumber;
 
     for (let i = 1; i <= gridArea; i++) {
-        const containerDiv = document.querySelector(".woah");
+        const containerDiv = document.querySelector(".container");
         let div = document.createElement("div");
         let pItem = p = document.createElement("p");
         div.style.cssText= "border: '2px'; border-color: red; border-style: solid; text-align: center;"
