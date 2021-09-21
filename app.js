6cef7a4e5bd3bd489b6acd886e64acb9c94b67fb
Jack Dillon <jack.dillon1@pcc.edu>
2021-09-21 11:41:35 -0700

    Rainbow option

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 let gridNumber = 16;
+let cellColor = 'black';
+let isRainbow = false;
+let rCell = 0;
+let cellColors = ['red', 'orange', 'yellow', 'green', 'blue', 'violet'];
 
 draw();
 
@@ -6,25 +10,55 @@ function draw(){
     document.getElementById("content").innerHTML = "";
     let gridArea = gridNumber * gridNumber;
     const containerDiv = document.querySelector(".container");
-    let w = 960 /gridNumber;
+    let w = 540 /gridNumber;
     for (let i = 1; i <= gridArea; i++) {
         let div = document.createElement("div");
         div.style.setProperty('width', w +"px");
         div.classList.add("plexel");
         containerDiv.appendChild(div);
-        div.addEventListener("mouseover", function moused() { 
-            div.style.setProperty('background-color', 'black');
+        div.addEventListener("mouseover", function moused() {
+            if(isRainbow){
+                div.style.setProperty('background-color', cellColors[rCell%6]);
+                rCell++;
+            } else {
+                div.style.setProperty('background-color', cellColor);
+            }
         ;});
     }
-
 }
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("myRange");
-output.innerHTML = slider.ariaValueMax;
+let slider = document.getElementById("myRange");
+let output = document.getElementById("outPut");
+output.innerHTML = slider.value;
 
 slider.oninput = function() {
-    output.innerHTML = this.ariaValueMax;
+    output.innerHTML = this.value;
     gridNumber = document.getElementById("myRange").value;
-    draw();
 }
+
+let clearButton = document.getElementById("clear");
+
+clearButton.addEventListener("click", function(){
+    document.getElementById("content").innerHTML = "";
+    draw();
+});
+
+let startButton = document.getElementById("start");
+
+startButton.addEventListener("click", function(){
+    draw();
+});
+
+let blackButton = document.getElementById("black");
+
+blackButton.addEventListener("click", function(){
+   cellColor = 'black';
+   isRainbow = false;
+});
+
+let rainbowButton = document.getElementById("rainbow");
+
+rainbowButton.addEventListener("click", function() {
+    rCell = 0;
+    isRainbow = true;
+});
