4d14eb0a6942cff84d5e83a6ec311a062087e4f2
jake <beej8046@gmail.com>
2021-09-20 17:38:35 -0800

    sketchem

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ let gridArea = gridNumber * gridNumber;
         let pItem = p = document.createElement("p");
         div.style.cssText= "border: '2px'; border-color: red; border-style: solid; text-align: center;"
     containerDiv.appendChild(div);
-
         pItem.textContent = 'divP';
+        div.addEventListener("mouseover", function moused() { div.style.cssText = "background-color: black;" })
     div.appendChild(pItem);
-
+        
     }
+    
